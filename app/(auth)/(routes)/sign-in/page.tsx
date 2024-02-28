db02830deda010e8134afec236595ff9454dc65b
shakhawatsalam <shakhawatsalam726@gmail.com>
2024-02-28 19:46:53 +0600

    [Clerk Setup Is Done]

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SignInPage() {
-  return <div>This Is an Unprotected Page</div>;
-}
-
-export default SignInPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,5 +1,8 @@
-import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <div>This is a procted page</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 }
