426d017d3c9f936199cf54354341ef1da3eb4ce0
shakhawatsalam <shakhawatsalam726@gmail.com>
2024-03-14 17:51:07 +0600

    [course category and course price part is done]

---
+++ script/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Quran" },
+        { name: "Business" },
+        { name: "Science" },
+        { name: "Farming" },
+        { name: "Fitness" },
+      ],
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
