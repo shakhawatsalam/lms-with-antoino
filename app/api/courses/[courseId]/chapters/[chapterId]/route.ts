e91a7b837be990d498dde885495b4c850349e0c0
shakhawatsalam <shakhawatsalam726@gmail.com>
2024-03-22 17:33:34 +0600

    [Video uploading is done with mux]

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
-import { db } from "@/lib/db";
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET,
+});
 
 export async function PATCH(
   req: Request,
@@ -40,7 +46,37 @@ export async function PATCH(
       },
     });
 
-    // TODO : HANDLE VIDEO UPLOAD
+    // * VIDEO UPLOADING
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const assets = await mux.video.assets.create({
+      input: values.videoUrl,
+      playback_policy: ["public"],
+      test: false,
+    });
+
+    await db.muxData.create({
+      data: {
+        chapterId: params.chapterId,
+        assetId: assets.id,
+        playbakcId: assets.playback_ids?.[0]?.id,
+      },
+    });
 
     return NextResponse.json(chapter);
   } catch (error) {
